cbe7a16398d5a0a78530ee67bded03218e215ddc
a.osetckiy <a.osetckiy@extracode.net>
2020-08-15 15:57:31 +0300

    Add sql file for structure db

---
+++ resource-server/sql/customers-db.sql
@@ -0,0 +1,92 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.7
+-- Dumped by pg_dump version 11.7
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: Customers; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Customers" (
+    id uuid NOT NULL,
+    title character varying(255),
+    is_deleted boolean,
+    created_at timestamp without time zone,
+    modified_at timestamp without time zone
+);
+
+
+ALTER TABLE public."Customers" OWNER TO postgres;
+
+--
+-- Name: Products; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public."Products" (
+    id uuid,
+    customer_id uuid,
+    title character varying(255),
+    description character varying(1024),
+    is_deleted boolean,
+    price numeric(10,2),
+    created_at timestamp,
+    modified_at timestamp
+)
+INHERITS (public."Customers");
+
+
+ALTER TABLE public."Products" OWNER TO postgres;
+
+--
+-- Data for Name: Customers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Customers" (id, title, is_deleted, created_at, modified_at) FROM stdin;
+\.
+
+
+--
+-- Data for Name: Products; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public."Products" (id, title, is_deleted, created_at, modified_at, customer_id, description, price) FROM stdin;
+\.
+
+
+--
+-- Name: Customers Customers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Customers"
+    ADD CONSTRAINT "Customers_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: Products FK_CUSTOMER_ID; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public."Products"
+    ADD CONSTRAINT "FK_CUSTOMER_ID" FOREIGN KEY (customer_id) REFERENCES public."Customers"(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
